94ac043e3de99c6086109d70af4e436a4a687040
Christian Kruse <cjk@defunct.ch>
2022-09-30 21:33:02 +0200

    basic navigation

--- src/App/Sidebar.tsx
+++ src/App/Sidebar.tsx
@@ -1,16 +1,37 @@
 import { Col } from "react-bootstrap";
 import { useTranslation } from "react-i18next";
 import Icon from "react-icons-kit";
-import { ic_grass, ic_home, ic_set_meal } from "react-icons-kit/md";
-import { Link } from "react-router-dom";
+import { ic_add, ic_grass, ic_home, ic_set_meal } from "react-icons-kit/md";
+import { Link, useLocation } from "react-router-dom";
 
-import { aboutPath, ingredientsPath, recipesPath, rootPath } from "../urls";
+import { aboutPath, ingredientsPath, newIngredientPath, newRecipePath, recipesPath, rootPath } from "../urls";
 
 export default function Sidebar() {
   const { t } = useTranslation(["root"]);
+  const { pathname } = useLocation();
+
+  const module = pathname.replace(/(?:(?:\d+(\/edit)?)|\/new)$/, "").replace(/.*\//, "");
 
   return (
     <Col as="nav" md={3} lg={2} className="sidebar d-flex flex-column flex-shrink-0 bg-light">
+      <ul className="nav nav-pills flex-column">
+        {module === "recipes" && (
+          <li className="nav-item">
+            <Link to={newRecipePath()} className="nav-link">
+              <Icon icon={ic_add} size="24" /> {t("root:sidebar.new_recipe")}
+            </Link>
+          </li>
+        )}
+
+        {module === "ingredients" && (
+          <li className="nav-item">
+            <Link to={newIngredientPath()} className="nav-link">
+              <Icon icon={ic_add} size="24" /> {t("root:sidebar.new_ingredient")}
+            </Link>
+          </li>
+        )}
+      </ul>
+
       <ul className="nav nav-pills flex-column mb-auto">
         <li className="nav-item">
           <Link to={rootPath()} className="nav-link">
--- src/urls/ingredients.ts
+++ src/urls/ingredients.ts
@@ -1,5 +1,6 @@
 import { TIngredient } from "../types";
 
 export const ingredientsPath = () => "/ingredients";
+export const newIngredientPath = () => "/ingredients/new";
 export const showIngredientPath = (ingredient: TIngredient) => `/ingredients/${ingredient.id}`;
 export const editIngredientPath = (ingredient: TIngredient) => `/ingredients/${ingredient.id}/edit`;
