13a5ed9b48ec03eae2ced6a88d9df0787c86ae08
Christian Kruse <cjk@defunct.ch>
2022-10-23 23:33:58 +0200

    Subnav for ingredients

--- src/features/Ingredients/index.tsx
+++ src/features/Ingredients/index.tsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 
+import { useSubnav } from "../../hooks";
 import Edit from "./Edit";
 import List from "./List";
 import New from "./New";
 import Show from "./Show";
+import IngredientsSubnav from "./Subnav";
 
 export default function IngredientsInterface() {
+  useSubnav(IngredientsSubnav);
+
   return (
     <Routes>
       <Route path="new" element={<New />} />
