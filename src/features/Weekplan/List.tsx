b0cc0d51d1909ce59637c55a10b5dd9442bf70bf
Christian Kruse <cjk@defunct.ch>
2023-01-07 20:05:40 +0100

    better visualization for week plan

--- src/features/Weekplan/List.tsx
+++ src/features/Weekplan/List.tsx
@@ -1,27 +1,32 @@
 import { useState } from "react";
 
 import { format, startOfISOWeek } from "date-fns";
-import { Button } from "react-bootstrap";
+import _ from "lodash";
+import { Button, ButtonGroup } from "react-bootstrap";
 import DatePicker from "react-date-picker";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import Icon from "react-icons-kit";
 import { ic_calendar_today, ic_swap_horiz } from "react-icons-kit/md";
+import { Link } from "react-router-dom";
 
 import { selectSession } from "../../App/sessionSlice";
-import { DeleteButton, FormGroup } from "../../components";
+import { DeleteButton, FormGroup, ShowButton } from "../../components";
+import { dateFormat, indexDate } from "../../dateUtils";
 import { DELETE_WEEKPLAN, LIST_WEEKPLAN_QUERY } from "../../graphql/weekplan";
 import { useAppDispatch, useAppSelector, useList, usePermissionFallback } from "../../hooks";
 import may from "../../permissions";
 import { TWeekplanEntry } from "../../types";
+import { showRecipePath } from "../../urls";
+import { recipeCalories, URI } from "../../utils";
+import { formatIntNumberRounded } from "../../utils/numbers";
 import MetaList from "../MetaList";
-import ListItem from "../Recipes/ListItem";
 import ReplaceModal from "./ReplaceModal";
 import { setReplaceModal } from "./weekplanSlice";
 
 export default function List() {
   const { user } = useAppSelector(selectSession);
   const [week, setWeek] = useState(startOfISOWeek(new Date()));
-  const { t } = useTranslation(["translation", "root"]);
+  const { t } = useTranslation(["translation", "root", "recipes"]);
   const dispatch = useAppDispatch();
 
   usePermissionFallback(may(user, "weekplan", "list"));
@@ -59,22 +64,46 @@ export default function List() {
         )}
       >
         {(entries) => (
-          <ul>
+          <ul className="weekplan-recipes-list">
             {entries.length === 0 && <li className="no-data">{t("translation:no_data")}</li>}
 
             {entries.map((entry) => (
-              <ListItem
-                key={entry.id}
-                recipe={entry.recipe}
-                buttons={
-                  <>
-                    <Button variant="secondary" onClick={() => replaceRecipe(entry)}>
-                      <Icon icon={ic_swap_horiz} /> {t("weekplan:list.replace")}
-                    </Button>
-                    <DeleteButton onClick={() => deleteItem(entry)}>{t("translation:delete")}</DeleteButton>
-                  </>
-                }
-              />
+              <li className="weekplan-list-item" key={entry.id}>
+                <div className="recipe-preview">
+                  {!!entry.recipe.image && <img src={`${URI}${entry.recipe.image.thumb}`} alt="" />}
+                </div>
+
+                <h3>
+                  <Link to={showRecipePath(entry.recipe)}>{entry.recipe.name}</Link>
+                </h3>
+
+                <Trans parent="span" className="calories" t={t} i18nKey="recipes:list.calories">
+                  {{ calories: formatIntNumberRounded(recipeCalories(entry.recipe).calories) }} kcal pro Portion
+                </Trans>
+                <span className="created">{dateFormat(entry.date, "EEEE, d.M.yyyy")}</span>
+
+                <ul className="recipes-recipes-show-tags-list">
+                  {_(entry.recipe.tags)
+                    .sortBy("name")
+                    .map((tag) => (
+                      <li className="tag" key={tag.id}>
+                        {tag.name}
+                      </li>
+                    ))
+                    .valueOf()}
+                </ul>
+
+                <ButtonGroup size="sm">
+                  <ShowButton as={Link} to={showRecipePath(entry.recipe)}>
+                    {t("translation:show")}
+                  </ShowButton>
+
+                  <Button variant="secondary" onClick={() => replaceRecipe(entry)}>
+                    <Icon icon={ic_swap_horiz} /> {t("weekplan:list.replace")}
+                  </Button>
+                  <DeleteButton onClick={() => deleteItem(entry)}>{t("translation:delete")}</DeleteButton>
+                </ButtonGroup>
+              </li>
             ))}
           </ul>
         )}
