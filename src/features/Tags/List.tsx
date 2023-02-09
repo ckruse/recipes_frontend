6c152bf76d0277f75b7508dfcbf55f9d8ab0e977
Christian Kruse <cjk@defunct.ch>
2023-02-09 20:44:40 +0100

    don’t limit tags in list, don’t use metalist for tags list

--- src/features/Tags/List.tsx
+++ src/features/Tags/List.tsx
@@ -1,39 +1,30 @@
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 
-import { TAG_DELETE_MUTATION, TAGS_COUNT_QUERY, TAGS_W_COUNT_QUERY } from "../../graphql/tags";
-import { useAppSelector, useList } from "../../hooks";
+import { TAG_DELETE_MUTATION, TAGS_W_COUNT_QUERY } from "../../graphql/tags";
+import { useList, useTitle } from "../../hooks";
 import { TTag } from "../../types";
 import { showTagPath } from "../../urls";
-import MetaList from "../MetaList";
 
 export default function List() {
-  const page = useAppSelector((state) => state.metaList.pages["tags"] || 0);
   const { t } = useTranslation(["tags"]);
 
-  const { items, count } = useList<TTag>({
+  const { items } = useList<TTag>({
     query: TAGS_W_COUNT_QUERY,
-    countQuery: TAGS_COUNT_QUERY,
     deleteMutation: TAG_DELETE_MUTATION,
-    variables: {
-      limit: 25,
-      offset: page * 25,
-    },
   });
 
+  useTitle(t("tags:list.title"));
+
   return (
-    <MetaList listKey="tags" items={items} count={count} title={t("tags:list.title")}>
-      {(tags) => (
-        <ul className="recipes-tags-list">
-          {tags.map((tag) => (
-            <li key={tag.id}>
-              <span className="tag">
-                <Link to={showTagPath(tag)}>{tag.name}</Link> ({tag.recipesCount})
-              </span>
-            </li>
-          ))}
-        </ul>
-      )}
-    </MetaList>
+    <ul className="recipes-tags-list">
+      {items?.map((tag) => (
+        <li key={tag.id}>
+          <span className="tag">
+            <Link to={showTagPath(tag)}>{tag.name}</Link> ({tag.recipesCount})
+          </span>
+        </li>
+      ))}
+    </ul>
   );
 }
