cef77854bafacce34aed529e7087bde8fdcfb575
Christian Kruse <cjk@defunct.ch>
2023-02-18 13:49:10 +0100

    fix ingredient permissions

--- src/permissions/IngredientPermissions.ts
+++ src/permissions/IngredientPermissions.ts
@@ -3,6 +3,7 @@ import { TIngredient } from "../types";
 
 const IngredientPermissions: PermissionBagType<TIngredient>["ingredients"] = {
   default: (user, resource) => true,
+  create: (user, resource) => !!user,
   edit: (user, resource) => user?.role === "ROOT",
   delete: (user, resource) => user?.role === "ROOT",
 };
